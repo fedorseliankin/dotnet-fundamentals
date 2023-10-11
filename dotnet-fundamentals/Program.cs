e42d8343fd27cb119e271b4388ff4fa7406fa569
fedor <Fedor_Seliankin@epam.com>
2023-10-11 13:35:09 +0600

    add library to the project

--- dotnet-fundamentals/Program.cs
+++ dotnet-fundamentals/Program.cs
@@ -1,6 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using hello_lib;
 string username = Console.ReadLine();
 Console.WriteLine("Hello, " + username);
 
-
+Class1.hello();
 
---
+++ hello-lib/Class1.cs
@@ -0,0 +1,11 @@
+﻿namespace hello_lib;
+
+public class Class1
+{
+    public static void hello()
+    {
+        string username = Console.ReadLine();
+        Console.WriteLine(DateTime.Now + "Hello, " + username);
+    }
+}
+
