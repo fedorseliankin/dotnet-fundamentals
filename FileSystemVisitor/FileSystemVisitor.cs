ec0b8e2ebd877f41c9a84b0054943976d76696a9
fedor <Fedor_Seliankin@epam.com>
2023-11-16 17:46:19 +0100

    add fileVisitor

---
+++ FileSystemVisitor/FileSystemVisitor.cs
@@ -0,0 +1,36 @@
+﻿namespace FileSystemVisitor;
+
+public class FileSystemVisitorCL
+{
+    private DirectoryInfo initDirectory;
+
+    public FileSystemVisitorCL(DirectoryInfo startDirectory)
+    {
+        initDirectory = startDirectory;
+    }
+
+    public IEnumerable<string> getEntries()
+    {
+        return getEntries(initDirectory);
+    }
+
+    public IEnumerable<string> getEntries(DirectoryInfo currentDirectory)
+    {
+        foreach (var fileSystemInfo in currentDirectory.EnumerateFileSystemInfos())
+        {
+            if (fileSystemInfo is FileInfo file)
+            {
+                yield return file.Name;
+            }
+            if (fileSystemInfo is DirectoryInfo directory)
+            {
+                yield return directory.Name;
+                foreach (string item in getEntries(directory))
+                {
+                    yield return item;
+                }
+            }
+        }
+    }
+}
+
