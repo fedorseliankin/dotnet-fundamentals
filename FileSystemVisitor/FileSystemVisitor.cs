d485fa1f2966a8d21b448dcae099a81bf93b78ba
fedor <Fedor_Seliankin@epam.com>
2023-11-30 18:27:51 +0600

    add abort and files/directories filter

--- FileSystemVisitor/FileSystemVisitor.cs
+++ FileSystemVisitor/FileSystemVisitor.cs
@@ -1,5 +1,18 @@
 ﻿namespace FileSystemVisitor;
 
+public enum CheckResult
+{
+    Contains,
+    Equals,
+    No
+}
+
+public enum AbortStatus
+{
+    No,
+    Wait,
+    Yes,
+}
 
 public class FileSystemVisitorCL
 {
@@ -8,24 +21,26 @@ public class FileSystemVisitorCL
         public string Name { get; set; }
     }
     private readonly DirectoryInfo initDirectory;
-    private readonly Func<string, bool>? filterFunc;
+    private readonly Func<string, CheckResult>? filterFunc;
+    private AbortStatus? abort;
+    private string? show;
 
-    private static readonly EventHandler<EventArgs> Start = (s, e) =>
+    private readonly EventHandler<EventArgs> Start = (s, e) =>
     {
         Console.WriteLine("Search started!");
     };
 
-    private static readonly EventHandler<EventArgs> Finish = (s, e) =>
+    private readonly EventHandler<EventArgs> Finish = (s, e) =>
     {
         Console.WriteLine("Search finished!");
     };
 
-    private static readonly EventHandler<FindedItemEvent> FileFound = (s, e) =>
+    private readonly EventHandler<FindedItemEvent> FileFound = (s, e) =>
     {
         Console.WriteLine("File Found: " + e.Name);
     };
 
-    private static readonly EventHandler<FindedItemEvent> DirFound = (s, e) =>
+    private readonly EventHandler<FindedItemEvent> DirFound = (s, e) =>
     {
         Console.WriteLine("Directory Found: " + e.Name);
     };
@@ -33,14 +48,29 @@ public class FileSystemVisitorCL
     public FileSystemVisitorCL(DirectoryInfo startDirectory)
     {
         initDirectory = startDirectory;
+        abort = AbortStatus.No;
+        show = "all";
     }
 
     public FileSystemVisitorCL(
         DirectoryInfo startDirectory,
-        Func<string, bool>? filterFunc
+        Func<string, CheckResult>? filterFunc,
+        string? show,
+        string? abort
     ) : this(startDirectory)
     {
         this.filterFunc = filterFunc;
+        if (abort is "y")
+        {
+            this.abort = AbortStatus.Wait;
+        } else if (abort is "n")
+        {
+            this.abort = AbortStatus.No;
+        } else
+        {
+            this.abort = AbortStatus.No;
+        }
+        this.show = show;
     }
 
     public IEnumerable<string> GetEntries()
@@ -60,19 +90,27 @@ public class FileSystemVisitorCL
             if (fileSystemInfo is FileInfo file)
             {
                 var name = CheckName(file);
-                if (name != null)
+                if (name != null && (show == "all" || show == "f"))
                 {
                     OnEvent(FileFound, new FindedItemEvent() {Name = name});
                     yield return name;
+                }
+                if (abort == AbortStatus.Yes)
+                {
+                    break;
                 }
             }
             if (fileSystemInfo is DirectoryInfo directory)
             {
                 var name = CheckName(directory);
-                if (name != null)
+                if (name != null && (show == "all" || show == "d"))
                 {
                     OnEvent(DirFound, new FindedItemEvent() { Name = name });
                     yield return name;
+                    if (abort == AbortStatus.Yes)
+                    {
+                        break;
+                    }
                 }
                 foreach (string item in GetEntries(directory))
                 {
@@ -84,7 +122,12 @@ public class FileSystemVisitorCL
 
     private string? CheckName(FileSystemInfo fileInfo)
     {
-        if (filterFunc == null || filterFunc(fileInfo.Name))
+        CheckResult checkResult = filterFunc(fileInfo.Name);
+        if (checkResult == CheckResult.Equals)
+        {
+            this.abort = AbortStatus.Yes;
+        }
+        if (filterFunc == null || checkResult != CheckResult.No)
         {
             return fileInfo.Name;
         }
--- dotnet-fundamentals/Program.cs
+++ dotnet-fundamentals/Program.cs
@@ -4,9 +4,24 @@ Console.WriteLine("Input directory path: ");
 string path = Console.ReadLine();
 Console.WriteLine("Input filtering string");
 string filter = Console.ReadLine();
+Console.WriteLine("Should abort on first fullmatch found? (y/n)");
+string shouldAbort = Console.ReadLine();
+Console.WriteLine("Should show files/directories/all items found? (f/d/all)");
+string show = Console.ReadLine();
 FileSystemVisitorCL fileSystemVisitor = new(
     new DirectoryInfo($@"{path}"),
-    filter is null or "" ? null : (string name) => name.Contains(filter)
+    filter is null or "" ? null : (string name) => {
+        if (name.Contains(filter)) {
+            if (name.Equals(filter))
+            {
+                return CheckResult.Equals;
+            }
+            return CheckResult.Contains;
+        }
+        return CheckResult.No;
+    },
+    show is null ? "" : show,
+    shouldAbort is null ? "n" : shouldAbort
 );
 
 foreach (string item in fileSystemVisitor.GetEntries())
