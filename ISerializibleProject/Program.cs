acef58c0360baad871a0aa71c1d8312aef07af87
fseliankin <fseliankin@support.connectionsacademy.org>
2024-02-02 11:53:43 +0600

    serialization

---
+++ ISerializibleProject/Program.cs
@@ -0,0 +1,44 @@
+﻿using System.Runtime.Serialization;
+using System.Runtime.Serialization.Formatters.Binary;
+
+[Serializable]
+public class MyClass : ISerializable
+{
+    public int Num { get; set; }
+    public string Str { get; set; }
+
+    public MyClass() { }
+
+    public MyClass(SerializationInfo info, StreamingContext context)
+    {
+        Num = info.GetInt32("Num");
+        Str = info.GetString("Str");
+    }
+
+    public void GetObjectData(SerializationInfo info, StreamingContext context)
+    {
+        info.AddValue("Num", Num);
+        info.AddValue("Str", Str);
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        MyClass myObject = new MyClass() { Num = 1, Str = "test" };
+
+        IFormatter formatter = new BinaryFormatter();
+        Stream stream = new FileStream("MyFile.bin", FileMode.Create, FileAccess.Write);
+
+        formatter.Serialize(stream, myObject);
+        stream.Close();
+
+        stream = new FileStream("MyFile.bin", FileMode.Open, FileAccess.Read);
+        MyClass deserializedObject = (MyClass)formatter.Deserialize(stream);
+
+        Console.WriteLine(deserializedObject.Num);
+        Console.WriteLine(deserializedObject.Str);
+        Console.ReadKey();
+    }
+}
---
+++ XMLSerialization/Program.cs
@@ -0,0 +1,49 @@
+﻿using System.Xml.Serialization;
+
+public class Employee
+{
+    [XmlElement(ElementName = "Name")]
+    public string EmployeeName { get; set; }
+}
+
+public class Department
+{
+    [XmlAttribute(AttributeName = "DeptName")]
+    public string DepartmentName { get; set; }
+
+    [XmlArray("Employees")]
+    [XmlArrayItem("Employee")]
+    public List<Employee> Employees { get; set; }
+}
+
+public class Program
+{
+    public static void Main()
+    {
+        var department = new Department
+        {
+            DepartmentName = "Sales",
+            Employees = new List<Employee>()
+            {
+                new Employee { EmployeeName = "John Doe" },
+                new Employee { EmployeeName = "Jane Doe" }
+            }
+        };
+
+        var formatter = new XmlSerializer(typeof(Department));
+
+        using (Stream stream = new FileStream("MyFile.xml", FileMode.Create, FileAccess.Write, FileShare.None))
+        {
+            formatter.Serialize(stream, department);
+        }
+
+        Department deserializedDepartment;
+
+        using (Stream stream = new FileStream("MyFile.xml", FileMode.Open, FileAccess.Read, FileShare.Read))
+        {
+            deserializedDepartment = (Department)formatter.Deserialize(stream);
+        }
+
+        Console.WriteLine($"Department: {deserializedDepartment.DepartmentName}, Employee: {deserializedDepartment.Employees[0].EmployeeName}");
+    }
+}
